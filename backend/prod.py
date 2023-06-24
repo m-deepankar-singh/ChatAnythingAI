508570d36ec7e2d3374c833d36d5503fa86b2107
Deepankar Singh <singh.deepankar39@gmail.com>
2023-06-25 00:46:54 +0530

    changes

--- backend/prod.py
+++ backend/prod.py
@@ -332,4 +332,7 @@ def delete_index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    host = '0.0.0.0'
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host=host, port=port, debug=True)
